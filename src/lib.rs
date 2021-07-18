9a593a102050aac149dc2187f35406906485d7bf
nhatvu148 <nhatvu148@gmail.com>
2021-07-18 12:50:58 +0900

    generic trait

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,37 @@ pub struct GBP(i32);
 #[derive(PartialEq, Debug)]
 pub struct CAD(i32);
 
-pub trait ToUSD {
-    fn to_usd(&self) -> USD;
-    // T implements FromUSD at compile time
-    fn convert<T: FromUSD>(&self) -> T {
-        T::from_usd(&self.to_usd())
+pub trait ToUSDv<F> {
+    fn to_uv(&self, f: F) -> f32;
+}
+pub trait FromUSDv<F> {
+    fn from_uv(&self, g: f32) -> F;
+}
+pub struct Ex {
+    cad: f32,
+    gbp: f32,
+}
+impl ToUSDv<GBP> for Ex {
+    fn to_uv(&self, g: GBP) -> f32 {
+        (g.0 as f32) * self.gbp
+    }
+}
+impl FromUSDv<CAD> for Ex {
+    fn from_uv(&self, f: f32) -> CAD {
+        CAD((f / self.cad) as i32)
     }
 }
 
-impl ToUSD for GBP {
-    fn to_usd(&self) -> USD {
-        USD((self.0 * 130) / 100)
-    }
+pub trait Exchange<F, T> {
+    fn convert(&self, f: F) -> T;
 }
 
-pub trait FromUSD {
-    fn from_usd(u: &USD) -> Self;
-}
-
-impl FromUSD for CAD {
-    fn from_usd(u: &USD) -> Self {
-        CAD((u.0 * 130) / 100)
+impl<E, F, T> Exchange<F, T> for E
+where
+    E: ToUSDv<F> + FromUSDv<T>,
+{
+    fn convert(&self, f: F) -> T {
+        self.from_uv(self.to_uv(f))
     }
 }
 
@@ -35,13 +45,11 @@ mod tests {
     #[test]
     fn it_works() {
         let g = GBP(200);
-        let u = g.to_usd();
-        assert_eq!(u, USD(260));
+        let ex = Ex { cad: 0.7, gbp: 1.3 };
 
-        let c = CAD::from_usd(&u);
-        assert_eq!(c, CAD(338));
+        // let c = ex.from_uv(ex.to_uv(g));
+        let c: CAD = ex.convert(g);
 
-        let c2: CAD = g.convert();
-        assert_eq!(c2, c);
+        assert_eq!(c, CAD(371));
     }
 }
